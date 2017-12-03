d78fbd15296d7d68d5710da12dfa5b112a848444
John Pham <johnphammail@gmail.com>
2017-12-02 16:19:29 -0800

    removing boilerplate

---
+++ frontend/src/components/BingoBoard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello">
+    <h1>ACM Bingo Board</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BingoBoard',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
